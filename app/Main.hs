d876e786932ceabe05098e5db1109603a4d6a0be
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-04-11 23:09:52 +0900

    Take the filename as a command line option

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
 import Brainfuck
+import Options.Applicative
+
+data Option = Option
+  { filename :: String
+  }
 
 main :: IO ()
-main = getContents >>= runBrainfuck . parseBrainfuck
+main = do
+  option <- execParser opts
+  readFile (filename option) >>= runBrainfuck . parseBrainfuck
+  where
+    bfOption :: Parser Option
+    bfOption = Option <$> argument str (metavar "FILE")
+
+    opts :: ParserInfo Option
+    opts = info (helper <*> bfOption)
+      ( fullDesc
+      <> progDesc "Brainfuck interpreter"
+      <> header "Brainfuck" )
+
